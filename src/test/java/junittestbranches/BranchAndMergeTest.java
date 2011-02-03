f59a33a393efaa6924d87d8b7f7c1e937b60f7fa
marciomazza <marciomazza@gmail.com>
2011-02-03 01:14:21 -0200

    simpler tests, now using JUnitCore.runClasses(...) directly

--- src/test/java/junittestbranches/BranchAndMergeTest.java
+++ src/test/java/junittestbranches/BranchAndMergeTest.java
@@ -1,27 +1,107 @@
 package junittestbranches;
 
-import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
 
-public class BranchAndMergeTest extends AbstractBranchRuleTest {
+/**
+ * Tests for {@link BranchRule} involving merge.
+ * 
+ * @author marciomazza
+ * 
+ */
+public class BranchAndMergeTest {
 
-	@Test
-	public void simpleBranch() throws Exception {
-
-		print("A");
-		brancher.branchAndMerge(new Runnable() {
-			public void run() {
-				print("1");
-			}
-		});
-		print("B");
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	public static class OneBranchAndMerge {
 
 		// Note that it continues after 1, because of the "merge"
-		checkWalkedPaths("A 1 B", "A B");
+		private static final String EXPECTED_LOG = " A1B AB";
+
+		private static String log = "";
+
+		@Rule
+		public BranchRule brancher = new BranchRule();
+
+		@Test
+		public void test() throws Exception {
+			log += " A";
+			brancher.branchAndMerge(new Runnable() {
+				public void run() {
+					log += "1";
+				}
+			});
+			log += "B";
+		}
 	}
 
+	public static class TwoBranchAndMerges {
+
+		// Note that it continues after 1 and 2, because of the "merge"
+		private static final String EXPECTED_LOG = " A1B2C AB2C ABC";
+		private static String log = "";
+
+		@Rule
+		public BranchRule brancher = new BranchRule();
+
+		@Test
+		public void test() throws Exception {
+			log += " A";
+			brancher.branchAndMerge(new Runnable() {
+				public void run() {
+					log += "1";
+				}
+			});
+			log += "B";
+			brancher.branchAndMerge(new Runnable() {
+				public void run() {
+					log += "2";
+				}
+			});
+			log += "C";
+		}
+	}
+
+	public static class MoreBranchAndMerges {
+
+		// Note that it continues after # and 3, because of the "merge"
+		private static final String EXPECTED_LOG = " A1#2 A12 AB3C ABC";
+		private static String log = "";
+
+		@Rule
+		public BranchRule brancher = new BranchRule();
+
+		@Test
+		public void test() throws Exception {
+			log += " A";
+			brancher.branch(new Runnable() {
+				public void run() {
+					log += "1";
+					brancher.branchAndMerge(new Runnable() {
+						public void run() {
+							log += "#";
+						}
+					});
+					log += "2";
+				}
+			});
+			log += "B";
+			brancher.branchAndMerge(new Runnable() {
+				public void run() {
+					log += "3";
+				}
+			});
+			log += "C";
+		}
+	}
+
+	@Test
+	public void test() {
+		OneBranchAndMerge.log = TwoBranchAndMerges.log = MoreBranchAndMerges.log = "";
+		JUnitCore.runClasses(OneBranchAndMerge.class, TwoBranchAndMerges.class, MoreBranchAndMerges.class);
+		assertEquals(OneBranchAndMerge.EXPECTED_LOG, OneBranchAndMerge.log);
+		assertEquals(TwoBranchAndMerges.EXPECTED_LOG, TwoBranchAndMerges.log);
+		assertEquals(MoreBranchAndMerges.EXPECTED_LOG, MoreBranchAndMerges.log);
+	}
 }
--- src/test/java/junittestbranches/BranchTest.java
+++ src/test/java/junittestbranches/BranchTest.java
@@ -1,26 +1,114 @@
 package junittestbranches;
 
-import org.junit.AfterClass;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-public class BranchTest extends AbstractBranchRuleTest {
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+
+/**
+ * Tests for {@link BranchRule}.
+ * 
+ * @author marciomazza
+ * 
+ */
+public class BranchTest {
+
+	public static class NoBranch {
+		private static final String EXPECTED_LOG = " A";
+		private static String log = "";
+
+		@Rule
+		public BranchRule brancher = new BranchRule();
+
+		@Test
+		public void test() throws Exception {
+			log += " A";
+		}
+	}
+
+	public static class OneBranch {
+		private static final String EXPECTED_LOG = " A1 AB";
+		private static String log = "";
+
+		@Rule
+		public BranchRule brancher = new BranchRule();
+
+		@Test
+		public void test() throws Exception {
+			log += " A";
+			brancher.branch(new Runnable() {
+				public void run() {
+					log += "1";
+				}
+			});
+			log += "B";
+		}
+	}
+
+	public static class TwoBranches {
+		private static final String EXPECTED_LOG = " A1 AB2 ABC";
+		private static String log = "";
+
+		@Rule
+		public BranchRule brancher = new BranchRule();
+
+		@Test
+		public void test() throws Exception {
+			log += " A";
+			brancher.branch(new Runnable() {
+				public void run() {
+					log += "1";
+				}
+			});
+			log += "B";
+			brancher.branch(new Runnable() {
+				public void run() {
+					log += "2";
+				}
+			});
+			log += "C";
+		}
+	}
+
+	public static class MoreBranches {
+		private static final String EXPECTED_LOG = " A1# A12 AB3 ABC";
+		private static String log = "";
+
+		@Rule
+		public BranchRule brancher = new BranchRule();
+
+		@Test
+		public void test() throws Exception {
+			log += " A";
+			brancher.branch(new Runnable() {
+				public void run() {
+					log += "1";
+					brancher.branch(new Runnable() {
+						public void run() {
+							log += "#";
+						}
+					});
+					log += "2";
+				}
+			});
+			log += "B";
+			brancher.branch(new Runnable() {
+				public void run() {
+					log += "3";
+				}
+			});
+			log += "C";
+		}
+	}
 
 	@Test
-	public void simpleBranch() throws Exception {
-
-		print("A");
-		brancher.branch(new Runnable() {
-			public void run() {
-				print("1");
-			}
-		});
-		print("B");
+	public void test() {
+		NoBranch.log = OneBranch.log = TwoBranches.log = MoreBranches.log = "";
+		JUnitCore.runClasses(NoBranch.class, OneBranch.class, TwoBranches.class, MoreBranches.class);
+		assertEquals(NoBranch.EXPECTED_LOG, NoBranch.log);
+		assertEquals(OneBranch.EXPECTED_LOG, OneBranch.log);
+		assertEquals(TwoBranches.EXPECTED_LOG, TwoBranches.log);
+		assertEquals(MoreBranches.EXPECTED_LOG, MoreBranches.log);
 	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-
-		checkWalkedPaths("A 1", "A B");
-	}
-
 }
